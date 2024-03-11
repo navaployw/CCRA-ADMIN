35613e95d1c6f9b24546318a539726a76db9b53e
navaployw <navaployw@users.noreply.github.com>
2024-03-11 11:49:08 +0700

    Delete app directory

--- app/mock.ts
+++
@@ -1,4 +0,0 @@
-export interface Mock {
-    id: number;
-    name: string;
-  }
--- app/utils/material-module.ts
+++
@@ -1,9 +0,0 @@
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {NgModule} from '@angular/core';
-@NgModule({
-  declarations: [],  
-  exports: [
-  	MatProgressSpinnerModule
-  ],
-})
-export class DemoMaterialModule {}  
