892263a690d92048ea19734eb9690de387219a70
dotnanoniwat <dotnano.niwat@gmail.com>
2024-04-03 09:50:55 +0700

    d

--- src/main/java/com/arg/ccra/adminonline/utils/SecretKeyProvider.java
+++ src/main/java/com/arg/ccra/adminonline/utils/SecretKeyProvider.java
@@ -1,10 +1,15 @@
 package com.arg.ccra.adminonline.utils;
 
+import org.springframework.beans.factory.annotation.Value;
 
 public interface SecretKeyProvider {
-    public static String PROVIDER_SECRET_KEY = "AES/ECB/PKCS5Padding";
-    public static String USER_CIPHER_ALGOR = "AES";
-    public static String USER_KEY_ALGOR = "AES";
-    public static String USER_KEY = "Y9cFfapTWdmk+7VNqjS1zor7p65xVkdPVAXLmtdmMhVOeiOoWGYeQJtbU8p6bDQe0FRURG9x9oR7FPbpt2fDGP+ieEoKa0gjEqw9DX5pgvufikX1TImdN0PwCrie54YvgMqpgSOZvM6sFUwNrqENR5M8n0qKaWSHomhdaJJNPi7AnMmdfSy8hPK0eScz8CYpIFya0CaZuwo8L7Dieqgpx7PNx3+WlRX3Tyiq3T1u5jeXOtPpngVYuBTjBaqFdCjPY++ejIh39vuAe/I1qo0jOhEOdulQxVmwwbfVJ6U3MrE=";
-
+    @Value("${com.arg.ccra.adminonline.utils.secretAlgor}")
+    public static String PROVIDER_SECRET_KEY = "";
+    @Value("${com.arg.ccra.adminonline.utils.secretAlgor}")
+    public static String USER_CIPHER_ALGOR = "";
+    @Value("${com.arg.ccra.adminonline.utils.userAlgor}")
+    public static String USER_KEY_ALGOR = "";
+    @Value("${com.arg.ccra.adminonline.utils.secretAlgor}")
+    public static String USER_KEY = "";
 }
+
